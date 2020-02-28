7b591a36e88f474290881798ba46edcf359f2d02
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-28 16:11:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -56,9 +56,9 @@ def reduce_to_total(source_array, starting_point = 0)
   total
 end
 
-# def reduce_to_all_true(source_array)
+def reduce_to_all_true(source_array)
   
-# end
+end
 
 # def reduce_to_any_true(source_array)
   
