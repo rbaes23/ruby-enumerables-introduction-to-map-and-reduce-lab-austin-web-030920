3fc72067aab16494c36d454426a9f02e6e5c9478
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-28 16:01:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,22 +1,54 @@
 def map_to_negativize(source_array)
-  source_array.map { |n| n * -1 }
+  new_array = []
+  i = 0
+  while i < source_array.length do
+    new_array << source_array[i] * -1
+    i += 1
+  end
+  new_array
 end
+
+# source_array.map { |n| n * -1 }
 
 def map_to_no_change(source_array)
-  source_array.map { |n| n * 1}
+  new_array = []
+  i = 0
+  while i < source_array.length do
+    new_array << source_array[i]
+    i += 1
+  end
+  new_array
 end
+
+# source_array.map { |n| n * 1}
 
 def map_to_double(source_array)
-  source_array.map { |n| n * 2 }
+  new_array = []
+  i = 0
+  while i < source_array.length do
+    new_array << source_array[i] * 2
+    i += 1
+  end
+  new_array
 end
+
+# source_array.map { |n| n * 2 }
 
 def map_to_square(source_array)
-  source_array.map { |n| n ** 2 }
+  new_array = []
+  i = 0
+  while i < source_array.length do
+    new_array << source_array[i] ** 2
+    i += 1
+  end
+  new_array
 end
 
-# def reduce_to_total(source_array, starting_point)
+# source_array.map { |n| n ** 2 }
+
+def reduce_to_total(source_array, starting_point)
   
-# end
+end
 
 # def reduce_to_all_true(source_array)
   
